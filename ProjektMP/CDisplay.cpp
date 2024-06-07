8d0dbe3fbad103cf4b9d4d75f9861c6147ec94e6
Katarzyna Trzaska <katarzyna.trzaska@student.pk.edu.pl>
2024-06-07 22:31:14 +0200

    Stworzenie Klasy Display

---
+++ ProjektMP/CDisplay.cpp
@@ -0,0 +1,13 @@
+#include "CDisplay.h"
+
+void CDisplay::drawTriangle(int a1, int a2, int b1, int b2)
+{
+}
+
+void CDisplay::drawCircle(int r)
+{
+}
+
+void CDisplay::drawParallelogram(int a1, int a2, int b1, int b2)
+{
+}
---
+++ ProjektMP/CDisplay.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef CDISPLAY_H
+#define CDISPLAY_H
+
+class CDisplay
+{
+public:
+	void drawTriangle(int a1, int a2, int b1, int b2);
+
+	void drawCircle(int r);
+
+	void drawParallelogram(int a1, int a2, int b1, int b2);
+};
+#endif // !CDISPLAY_H
